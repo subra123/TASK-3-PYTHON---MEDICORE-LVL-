241b7e2c887e0b806334646da588bf501ca60481
subra123 <97328633+subra123@users.noreply.github.com>
2022-12-10 09:37:56 +0530

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,14 +1,14 @@
 
-file1 = open('onelinefile.txt', 'r')
-
-file2 = open('Filename2.csv','w')
-
-content = file1.read()
-
-content = content.split()
-
-for i in range(0, len(content), 4):
-    file2.write(content[i] + ',' + content[i+1] + ',' + content[i+2] + ',' + content[i+3] + '\n')
-
-file1.close()
-file2.close()
+with open('about.txt', 'r') as f:
+    words = f.read().split()
+    long_words = [word for word in words if len(word) >= 6]
+    word_frequency = {}
+    for word in words:
+        if word in word_frequency:
+            word_frequency[word] += 1
+        else:
+            word_frequency[word] = 1
+    
+    most_frequent_word = max(word_frequency, key=word_frequency.get)
+    print("The words with atleast 6 letters are: ", long_words)
+    print('The most frequent word is: ', most_frequent_word)
